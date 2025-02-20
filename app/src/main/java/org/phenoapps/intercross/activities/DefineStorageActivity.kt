db656a97275440c447ae10d437e6640bf98568c1
kamathprasad9 <54414375+kamathprasad9@users.noreply.github.com>
2025-02-20 17:36:36 -0500

    Add windowInsetListeners to activities

--- app/src/main/java/org/phenoapps/intercross/activities/DefineStorageActivity.kt
+++ app/src/main/java/org/phenoapps/intercross/activities/DefineStorageActivity.kt
@@ -2,7 +2,12 @@ package org.phenoapps.intercross.activities
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import dagger.hilt.android.AndroidEntryPoint
 import org.phenoapps.intercross.R
 import org.phenoapps.utils.BaseDocumentTreeUtil
@@ -13,8 +18,18 @@ class DefineStorageActivity: AppCompatActivity() {
     private var mBackButtonEnabled = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
+        window.apply {
+            WindowCompat.getInsetsController(this, decorView).apply {
+                isAppearanceLightStatusBars = true
+            }
+        }
+
         setContentView(R.layout.activity_define_storage)
+
+        setWindowInsetListener()
     }
 
     override fun onBackPressed() {
@@ -30,5 +45,19 @@ class DefineStorageActivity: AppCompatActivity() {
 
     fun enableBackButton(enable: Boolean) {
         mBackButtonEnabled = enable
+    }
+
+    private fun setWindowInsetListener() {
+        window.decorView.findViewById<View>(android.R.id.content)?.let {
+            ViewCompat.setOnApplyWindowInsetsListener(it) { _, windowInsets ->
+                val insets = windowInsets.getInsets(
+                    WindowInsetsCompat.Type.displayCutout() or WindowInsetsCompat.Type.systemBars()
+                )
+
+                it.setPadding(0, insets.top, 0, insets.bottom)
+
+                windowInsets
+            }
+        }
     }
 }
